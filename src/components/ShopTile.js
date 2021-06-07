156c9a8fe4e071123f1652d6e1c7244f295d10f9
Jason Aravanis <j.aravanis@icloud.com>
2021-06-07 17:44:31 +1000

    Add basic grid layout of shopTile

--- src/components/ShopTile.js
+++ src/components/ShopTile.js
@@ -3,31 +3,47 @@ import styled from "styled-components";
 import { useSpring, animated } from "react-spring";
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 5rem;
-  height: 5rem;
+  width: 20rem;
+  height: 25rem;
+  display: grid;
+  grid-template-rows: 80% 10% 10%;
+  grid-template-columns: auto;
+  grid-template-areas: "image" "category" "title";
 `;
 
 const ImageContainer = styled.div`
-  min-width: 100%;
-  min-height: 100%;
+  width: 100%;
+  grid-area: image;
   position: relative;
 `;
 
 const ImagePrimary = styled(animated.img)`
-  min-width: 100%;
-  min-height: 100%;
+  width: 100%;
   position: absolute;
+  z-index: 1;
 `;
 
 const ImageSecondary = styled.img`
-  min-width: 100%;
-  min-height: 100%;
-  posotion: absolute;
-  opacity: 1;
+  width: 100%;
+  position: absolute;
+`;
+
+const Category = styled.span`
+  grid-area: category;
+  justify-self: center;
+  margin: 0.5rem;
+  color: #1e73be;
+  font-size: 1rem;
+  z-index: 2;
+`;
+
+const Title = styled.span`
+  grid-area: title;
+  justify-self: center;
+  color: "black";
+  font-size: 1.5rem;
+  font-family: Tungsten;
+  z-index: 2;
 `;
 
 function ShopTile({ item }) {
@@ -54,6 +70,9 @@ function ShopTile({ item }) {
         />
         <ImageSecondary src={item.secondaryImage} />
       </ImageContainer>
+
+      <Category>{item.category}</Category>
+      <Title>{item.title}</Title>
     </Container>
   );
 }
