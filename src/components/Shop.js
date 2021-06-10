73e2ad24efe724beaddf0886e7f5824833bfe64a
Jason Aravanis <j.aravanis@icloud.com>
2021-06-10 23:13:07 +1000

    Add basic shop page layout

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,7 +1,50 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SectionHeader = styled.h2`
+  color: white;
+  font-family: Tungsten;
+  font-size: 2rem;
+  margin: 2rem;
+`;
+
+const Grid = styled.div`
+  width: 90%;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
+  grid-gap: 1rem;
+`;
+
+const PlaceHolder = styled.div`
+  background: white;
+  width: 100%;
+  height: 25rem;
+`;
 
 function Shop() {
-  return <div>THIS IS THE SHOP COMPONENT</div>;
+  return (
+    <Container>
+      <SectionHeader>SHOP</SectionHeader>
+      <Grid>
+        <PlaceHolder />
+        <PlaceHolder />
+        <PlaceHolder />
+        <PlaceHolder />
+        <PlaceHolder />
+        <PlaceHolder />
+        <PlaceHolder />
+        <PlaceHolder />
+      </Grid>
+    </Container>
+  );
 }
 
 export default Shop;
